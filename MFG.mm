1c1d79648ed776622f8f06a08347f88238c1434b
Louis-Benjamin Durocher <benfit34@apexsciences.com>
2018-05-09 18:17:31 -0400

    Add files via upload

---
+++ MFG.mm
@@ -0,0 +1,79 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1525781272112" ID="ID_1823414166" MODIFIED="1525795092784" TEXT="MFG">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1525781496154" ID="ID_1023014430" MODIFIED="1525788758607" POSITION="right" TEXT="1. Youtube video - Freemind (20 min)">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font ITALIC="true" NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1525781498583" ID="ID_1635878424" MODIFIED="1525788758612" POSITION="right" TEXT="2. Text Editor for php">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1525781500630" HGAP="37" ID="ID_1236604483" MODIFIED="1525788758613" POSITION="right" TEXT="3. Apps" VSHIFT="5">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1525781786962" ID="ID_1985608718" MODIFIED="1525788758617" TEXT="phpMyAdmin">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1525782554428" HGAP="26" ID="ID_1863719110" MODIFIED="1525788758617" TEXT="internet.sql" VSHIFT="-19">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1525781798987" HGAP="26" ID="ID_1954437511" MODIFIED="1525788758622" TEXT="Bootstrap" VSHIFT="17">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1525781802891" ID="ID_777097815" MODIFIED="1525788758623" TEXT="Codecharge">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1525782721141" ID="ID_1397125999" MODIFIED="1525788758624" TEXT="use mySQL -&gt; collection table">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1525781806924" ID="ID_460138185" MODIFIED="1525788758627" TEXT="js tree">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1525781501207" HGAP="64" ID="ID_425607848" MODIFIED="1525788869924" POSITION="right" TEXT="CSS" VSHIFT="-26">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<arrowlink DESTINATION="ID_425607848" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1138106942" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_425607848" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1138106942" SOURCE="ID_425607848" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1525781497142" ID="ID_1693995593" MODIFIED="1525788758630" POSITION="right" TEXT="Json">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1525782396610" HGAP="47" ID="ID_1275613222" MODIFIED="1525795144616" POSITION="left" TEXT="Diebec" VSHIFT="-60">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1525795107280" ID="ID_405211153" MODIFIED="1525795123905" TEXT="Steel Rule Cutting Dies">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1525795127322" ID="ID_573651654" MODIFIED="1525795136849" TEXT="Counter Plates">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1525795145963" ID="ID_1369459879" MODIFIED="1525795171431" TEXT="Stripping Tools">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1525795179625" ID="ID_1463086178" MODIFIED="1525795192171" TEXT="Blanking Tools">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1525795180616" ID="ID_276801055" MODIFIED="1525795199324" TEXT="Embossing Tools">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1525784237921" ID="ID_152771736" MODIFIED="1525788758639" POSITION="left" TEXT="SOFTWARE DEV PLAN -&gt; IEEE">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
